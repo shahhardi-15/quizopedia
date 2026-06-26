1231c1b6f88eb4d17ce3699bf756a0e6798a84b3
Hardi Shah <hardishah1505@gmail.com>
2026-06-26 15:01:40 +0530

    Fix email indentation and switch to SSL port 465

--- stu_test/settings.py
+++ stu_test/settings.py
@@ -108,8 +108,8 @@ AUTHENTICATION_BACKENDS = [
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = f'Quizopedia <{EMAIL_HOST_USER}>'
